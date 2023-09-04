723d26250ba31924566d36ddd320476f565a22c4
Artemisssm <2651474519@qq.com>
2023-09-04 07:43:56 -0400

    one-resnet

---
+++ train.py
@@ -0,0 +1,209 @@
+import os
+from config import *
+import utils
+from wvae import *
+from torch import optim
+from tqdm import tqdm
+from utils import *
+from torchvision.utils import save_image
+from torch.nn import functional as F
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+device = torch.device('cuda')
+args = get_config()
+
+if 'pendulum' in args.dataset:
+    label_idx = range(4)
+elif 'human' in args.dataset:
+    label_idx = range(6)
+elif 'tree' in args.dataset:
+    label_idx = range(7)
+else:
+    if args.labels == 'smile':
+        label_idx = [31, 20, 19, 21, 23, 13]
+    elif args.labels == 'age':
+        label_idx = [39, 20, 28, 18, 13, 3]
+    else:
+        raise NotImplementedError("Not supported structure.")
+num_label = len(label_idx)
+
+save_dir = './results/{}/{}_{}_sup{}/'.format(
+    args.dataset, args.labels, args.prior, str(args.sup_type))
+utils.make_folder(save_dir)
+utils.write_config_to_file(args, save_dir)
+
+celoss = torch.nn.BCEWithLogitsLoss()
+
+# 得到数据集
+train_loader, test_loader, train_set = utils.make_dataloader(args)
+log_file_name = os.path.join(save_dir, 'log.txt')
+
+if args.resume:
+    log_file = open(log_file_name, "at")
+else:
+    log_file = open(log_file_name, "wt")
+
+if 'scm' in args.prior:
+    A = torch.zeros((num_label, num_label))
+    if args.labels == 'smile':
+        A[0, 2:6] = 1
+        A[1, 4] = 1
+    elif args.labels == 'age':
+        A[0, 2:6] = 1
+        A[1, 2:4] = 1
+    elif args.labels == 'pend':
+        A[0, 2:4] = 1
+        A[1, 2:4] = 1
+    elif args.labels == 'hum':
+        A[0, 3:6] = 1
+        A[1, 2:6] = 1
+    elif args.labels == 'tre':
+        A[0, 4:6] = 1
+        A[1, 2:4] = 1
+        A[3, 4:7] = 1
+else:
+    A = None
+
+
+print('Build models...')
+model = WVAE(args.latent_dim, args.g_conv_dim, args.image_size,
+            args.enc_dist, args.enc_arch, args.enc_fc_size, args.enc_noise_dim, args.dec_dist,
+            args.prior, num_label, A, args.anneal_steps, args.alpha, args.beta, args.gamma)
+
+A_optimizer = None
+prior_optimizer = None
+if 'scm' in args.prior:
+    enc_param = model.encoder.parameters()  # 获取模型的编码器的参数，并赋值给enc_param
+    dec_param = model.decoder.parameters()  # 获取模型的解码器的参数，并转换成列表赋值给dec_param
+    prior_param = list(model.prior.parameters())  # 获取模型的先验的参数，并转换成列表赋值给prior_param
+    A_optimizer = optim.Adam(prior_param[0:1], lr=args.lr_a)  # 创建一个Adam优化器，用来优化prior_param中的第一个参数（即A），并赋值给A_optimizer
+    prior_optimizer = optim.Adam(prior_param[1:], lr=args.lr_p, betas=(args.beta1, args.beta2))  # 创建一个Adam优化器，用来优化prior_param中的其余参数，并赋值给prior_optimizer
+else:
+    enc_param = model.encoder.parameters()
+    dec_param = model.decoder.parameters()
+
+encoder_optimizer = optim.Adam(enc_param, lr=args.lr_e, betas=(args.beta1, args.beta2)) # 创建一个Adam优化器，用来优化enc_param中的参数，并赋值给encoder_optimizer
+decoder_optimizer = optim.Adam(dec_param, lr=args.lr_g, betas=(args.beta1, args.beta2)) # 创建一个Adam优化器，用来优化dec_param中的参数，并赋值给decoder_optimizer
+
+model = nn.DataParallel(model.to(device))
+
+if args.prior == 'uniform':  # 如果先验分布是均匀分布
+    fixed_noise = torch.rand(args.save_n_samples, args.latent_dim,
+                             device=device) * 2 - 1  # 生成一个服从[-1,1]区间的随机张量，并赋值给fixed_noise
+else:  # 否则
+    fixed_noise = torch.randn(args.save_n_samples, args.latent_dim, device=device)  # 生成一个服从标准正态分布的随机张量，并赋值给fixed_noise
+fixed_unif_noise = torch.rand(1, args.latent_dim, device=device) * 2 - 1  # 生成一个服从[-1,1]区间的随机张量，并赋值给fixed_unif_noise
+fixed_zeros = torch.zeros(1, args.latent_dim, device=device)  # 生成一个全零张量，并赋值给fixed_zeros
+
+def test(epoch, i, model, test_data, save_dir, fixed_noise, fixed_zeros):
+    model.eval()
+    with torch.no_grad(): # 不计算梯度
+        x = test_data.to(device) # 把测试数据转移到device上，并赋值给x
+
+        # Reconstruction
+        x_recon = model(x, recon=True) # 调用模型，得到重建后的图像，并赋值给x_recon
+        recons = utils.draw_recon(x.cpu(), x_recon.cpu()) # 调用utils.draw_recon函数，绘制原始图像和重建图像的对比，并赋值给recons
+        del x_recon # 删除x_recon，释放内存
+        save_image(recons, save_dir + 'recon_' + str(epoch) + '_' + str(i) + '.png', nrow=args.nrow,
+                   normalize=True, scale_each=True) # 保存recons到指定的路径，使用args.nrow指定每行的图片数量，使用normalize和scale_each进行归一化
+
+        # Generation
+        sample = model(z=fixed_noise).cpu() # 调用模型，得到生成的图像，并转移到cpu上，并赋值给sample
+        save_image(sample, save_dir + 'gen_' + str(epoch) + '_' + str(i) + '.png', normalize=True, scale_each=True) # 保存sample到指定的路径，使用normalize和scale_each进行归一化
+
+        # Traversal (given a fixed traversal range)
+        sample = model.module.traverse(fixed_zeros).cpu() # 调用model.module.traverse函数，得到遍历隐变量后的图像，并转移到cpu上，并赋值给sample
+        save_image(sample, save_dir + 'trav_' + str(epoch) + '_' + str(i) + '.png', normalize=True, scale_each=True, nrow=10) # 保存sample到指定的路径，使用normalize和scale_each进行归一化，使用nrow=10指定每行的图片数量
+        del sample # 删除sample，释放内存
+
+    model.train()
+
+print('Start training...')
+for epoch in range(args.start_epoch, args.start_epoch + args.n_epochs):
+    pbar = tqdm(train_loader, total=len(train_loader), desc=f'Epoch {epoch}')
+    model.train()
+    for batch_idx, (x, label) in enumerate(pbar):
+        x = x.to(device)
+        sup_flag = label[:, 0] != -1 # 判断label的第一列是否不等于-1，得到一个布尔张量，并赋值给sup_flag
+        if sup_flag.sum() > 0: # 如果sup_flag中为True的元素个数大于0，说明有有效的标签
+            label = label[sup_flag, :][:, label_idx].float() # 用sup_flag筛选出有效的标签，并用label_idx选择需要的列，然后转换成浮点类型，并赋值给label
+        if 'pendulum' or 'tree' in args.dataset: # 如果args.dataset中包含'pendulum'或者'human'
+            if args.sup_type == 'ce': # 如果args.sup_type是'ce'
+                # Normalize labels to 0,1
+                scale = get_scale(train_set)  # 调用get_scale函数，获取训练集的最小值和最大值，并赋值给scale
+                label = (label - scale[0]) / (scale[1] - scale[0])  # 用scale对label进行归一化，使其范围在0到1之间，并赋值给label
+            else:  # 否则
+                # Normalize labels to mean 0 std 1
+                mm, ss = get_stats()  # 调用get_stats函数，获取训练集的均值和标准差，并赋值给mm和ss
+                label = (label - mm) / ss  # 用mm和ss对label进行标准化，使其均值为0，标准差为1，并赋值给label
+            num_labels = len(label_idx)  # 获取label_idx的长度，并赋值给num_labels
+            label = label.to(device)  # 把label转移到device上，并赋值给label
+
+        if 'scm' in args.prior:
+            z_fake, x_fake, z, z_fake_mean, z_fake_logvar = model(x)
+        else:
+            z_fake, x_fake, z_fake_mean, z_fake_logvar = model(x)
+
+        # train model
+        # model.zero_grad()
+        if sup_flag.sum() > 0:  # 如果sup_flag中为True的元素个数大于0，说明有有效的标签
+            label_z = z[sup_flag, :num_labels]  # 用sup_flag筛选出有效的隐变量，并用num_labels选择需要的列，并赋值给label_z
+            if 'pendulum' or 'tree' in args.dataset:  # 如果args.dataset中包含'pendulum'
+                if args.sup_type == 'ce':  # 如果args.sup_type是'ce'
+                    # CE loss
+                    sup_loss = celoss(label_z, label)  # 计算label_z和label之间的交叉熵损失，并赋值给sup_loss
+                else:  # 否则
+                    # l2 loss
+                    sup_loss = nn.MSELoss()(label_z, label)  # 计算label_z和label之间的均方误差损失，并赋值给sup_loss
+            else:  # 否则
+                sup_loss = celoss(label_z, label)  # 计算label_z和label之间的交叉熵损失，并赋值给sup_loss，label是经过先验之后得到的标签，label_z是刚得到的标签
+        else:  # 否则
+            sup_loss = torch.zeros([1], device=device)  # 生成一个全零张量，并赋值给sup_loss
+
+
+        loss_encoder = sup_loss * args.sup_coef # 把loss_encoder和sup_loss乘以args.sup_coef的结果相加，并赋值给loss_encoder
+
+        loss_decoder = model.module.loss_function(x_fake, x, z_fake_mean, z_fake_logvar, z)['loss']
+        loss = loss_encoder + loss_decoder
+        model.module.zero_grad()
+        if epoch == 1:
+            if batch_idx != 0:
+                model.module.prior.set_zero_grad()
+        else:
+            model.module.prior.set_zero_grad()
+        loss.backward()
+        encoder_optimizer.step()
+        decoder_optimizer.step()
+        if 'scm' in args.prior:  # 如果args.prior中包含'scm'
+            A_optimizer.step()  # 调用A_optimizer，更新因果层的参数
+            prior_optimizer.step()  # 调用prior_optimizer，更新先验网络的参数
+
+        if batch_idx == 0 or (batch_idx + 1) % args.print_every == 0:
+            log = ('Train Epoch: {} ({:.0f}%)\t, Encoder loss: {:.4f}, Decoder loss: {:.4f}, Sup loss: {:.4f}'.format(
+                epoch, 100. * batch_idx / len(train_loader),
+                loss_encoder.item(), loss_decoder.item(), sup_loss.item()))
+            print(log)
+            log_file.write(log + '\n')
+            log_file.flush()
+
+        if (epoch == 1 or epoch % args.sample_every_epoch == 0) and batch_idx == len(train_loader) - 1: # 如果epoch是1或者能被args.sample_every_epoch整除，并且batch_idx是最后一个批次的索引
+            test(epoch, batch_idx + 1, model, x[:args.save_n_recons], save_dir, fixed_noise, fixed_zeros) # 调用test函数，传入epoch, batch_idx + 1, model, x的前args.save_n_recons
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ wvae.py
@@ -0,0 +1,293 @@
+import torch.nn as nn
+from resnet import *
+import torch
+from sagan import *
+from causal_model import *
+import math
+
+
+class Encoder(nn.Module):
+    r'''ResNet Encoder
+
+    Args:
+        latent_dim: latent dimension
+        arch: network architecture. Choices: resnet - resnet50, resnet18
+        dist: encoder distribution. Choices: deterministic, gaussian, implicit
+        fc_size: number of nodes in each fc layer
+        noise_dim: dimension of input noise when an implicit encoder is used
+    '''
+
+    def __init__(self, latent_dim=64, arch='resnet', dist='gaussian', fc_size=2048, noise_dim=128):
+        super().__init__()  # 调用父类的初始化函数
+        self.latent_dim = latent_dim  # 把隐变量的维度赋值给self.latent_dim
+        self.dist = dist  # 把隐变量的分布赋值给self.dist
+        self.noise_dim = noise_dim  # 把噪声的维度赋值给self.noise_dim
+
+        in_channels = noise_dim + 3 if dist == 'implicit' else 3  # 根据dist的值，确定输入通道的数量
+        out_dim = latent_dim * 2 if dist == 'gaussian' else latent_dim  # 根据dist的值，确定输出维度的大小
+        if arch == 'resnet':  # 如果架构是'resnet'
+            self.encoder = resnet50(pretrained=False, in_channels=in_channels, fc_size=fc_size,
+                                    out_dim=out_dim)  # 创建一个预训练的resnet50编码器，并把它赋值给self.encoder
+        else:  # 否则
+            assert arch == 'resnet18'  # 断言架构是'resnet18'
+            self.encoder = resnet18(pretrained=False, in_channels=in_channels, fc_size=fc_size,
+                                    out_dim=out_dim)  # 创建一个预训练的resnet18编码器，并把它赋值给self.encoder
+
+    def forward(self, x, avepool=False):
+        '''
+        :param x: input image
+        :param avepool: whether to return the average pooling feature (used for downstream tasks)
+        :return:
+        '''
+        if self.dist == 'implicit':  # 如果self.dist是'implicit'
+            # Concatenate noise with the input image x
+            noise = x.new(x.size(0), self.noise_dim, 1, 1).normal_(0, 1)  # 生成一个服从标准正态分布的随机张量，并赋值给noise
+            noise = noise.expand(x.size(0), self.noise_dim, x.size(2), x.size(3))  # 把noise扩展到和x相同的维度，并赋值给noise
+            x = torch.cat([x, noise], dim=1)  # 把x和noise在第二个维度上拼接起来，并赋值给x
+        z, ap = self.encoder(x)  # 调用self.encoder，得到隐变量z和平均池化后的输出ap，并赋值给相应的变量
+        if avepool:  # 如果avepool为真
+            return ap  # 返回ap
+        if self.dist == 'gaussian':  # 如果self.dist是'gaussian'
+            return z.chunk(2, dim=1)  # 把z在第二个维度上分成两个张量，并返回
+        else:  # 否则
+            return z  # 返回z
+
+
+# encoder = Encoder()
+# # print((encoder))
+# x = torch.rand(16, 3, 64, 64)
+# z_mu, z_a = encoder(x)
+# print(z_mu, z_a)
+
+
+class Decoder(nn.Module):
+    r'''Big generator based on SAGAN
+
+    Args:
+        latent_dim: latent dimension
+        conv_dim: base number of channels
+        image_size: image resolution
+        dist: generator distribution. Choices: deterministic, gaussian, implicit
+        g_std: scaling the standard deviation of the gaussian generator. Default: 1
+    '''
+
+    def __init__(self, latent_dim=64, conv_dim=32, image_size=64, dist='deterministic', g_std=1):
+        super().__init__()
+        self.latent_dim = latent_dim  # 把隐变量的维度赋值给self.latent_dim
+        self.dist = dist  # 把隐变量的分布赋值给self.dist
+        self.g_std = g_std  # 把高斯分布的标准差赋值给self.g_std
+
+        out_channels = 6 if dist == 'gaussian' else 3  # 根据dist的值，确定输出通道的数量
+        add_noise = True if dist == 'implicit' else False  # 根据dist的值，确定是否在解码器中添加噪声
+        self.decoder = Generator(latent_dim, conv_dim, image_size, out_channels,
+                                 add_noise)  # 创建一个生成器，并把它赋值给self.decoder
+
+    def forward(self, z, mean=False, stats=False):
+        out = self.decoder(z)  # 调用self.decoder，得到输出，并赋值给out
+        if self.dist == 'gaussian':  # 如果self.dist是'gaussian'
+            x_mu, x_logvar = out.chunk(2, dim=1)  # 把out在第二个维度上分成两个张量，并赋值给x_mu和x_logvar
+            if stats:  # 如果stats为真
+                return x_mu, x_logvar  # 返回x_mu和x_logvar
+            else:  # 否则
+                x_sample = reparameterize(x_mu, (x_logvar / 2).exp(),
+                                          self.g_std)  # 调用reparameterize函数，根据x_mu和x_logvar生成样本，并赋值给x_sample
+                if mean:  # 如果mean为真
+                    return x_mu  # 返回x_mu
+                else:  # 否则
+                    return x_sample  # 返回x_sample
+        else:  # 否则
+            return out  # 返回out
+
+
+# decoder = Decoder()
+# z = torch.rand(16, 64)
+# x = decoder(z)
+# print(x)
+
+
+def reparameterize(mu, sigma, std=1):
+    assert mu.shape == sigma.shape
+    eps = mu.new(mu.shape).normal_(0, std)
+    return mu + sigma * eps
+
+
+class WVAE(nn.Module):
+    num_iter = 0
+    def __init__(self, latent_dim=64, conv_dim=32, image_size=64,
+                 enc_dist='gaussian', enc_arch='resnet', enc_fc_size=2048, enc_noise_dim=128, dec_dist='implicit',
+                 prior='gaussian', num_label=None, A=None, anneal_steps=200, alpha=1, beta=6, gamma=1):
+        super().__init__()
+        self.latent_dim = latent_dim
+        self.image_size = image_size
+        self.enc_dist = enc_dist
+        self.dec_dist = dec_dist
+        self.prior_dist = prior
+        self.num_label = num_label
+        self.anneal_steps = anneal_steps
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+
+
+        self.encoder = Encoder(latent_dim, enc_arch, enc_dist, enc_fc_size, enc_noise_dim)
+        self.decoder = Decoder(latent_dim, conv_dim, image_size, dec_dist)
+        if 'scm' in prior:
+            self.prior = SCM(num_label, A, scm_type=prior)
+
+    def encode(self, x, mean=False, avepool=False):
+        if avepool:
+            return self.encoder(x, avepool=True)
+        else:
+            if self.enc_dist == 'gaussian':
+                z_mu, z_logvar = self.encoder(x)
+                if mean:  # for downstream tasks
+                    return z_mu
+                else:
+                    z_fake = reparameterize(z_mu, (z_logvar / 2).exp())
+                    return z_fake
+            else:
+                return self.encoder(x)
+
+    def decode(self, z, mean=True):
+        if self.decoder_type != 'gaussian':
+            return self.decoder(z)
+        else:  # gaussian
+            return self.decoder(z, mu=mean)
+
+    def traverse(self, eps, gap=6, n=10):
+        dim = self.num_label if self.num_label is not None else self.latent_dim  # 如果self.num_label不是None，就把它赋值给dim，否则就把self.latent_dim赋值给dim
+        sample = torch.zeros((n * dim, 3, self.image_size, self.image_size))  # 生成一个全零张量，并赋值给sample
+        eps = eps.expand(n, self.latent_dim)  # 把eps扩展到(n, self.latent_dim)的维度，并赋值给eps
+        if self.prior_dist == 'gaussian' or self.prior_dist == 'uniform':  # 如果self.prior_dist是'gaussian'或者'uniform'
+            z = eps  # 把eps赋值给z
+        else:  # 否则
+            label_z = self.prior(eps[:, :dim])  # 调用self.prior，得到eps的前dim个维度经过因果层后的输出，并赋值给label_z
+            other_z = eps[:, dim:]  # 得到eps的剩余维度，并赋值给other_z
+            z = torch.cat([label_z, other_z], dim=1)  # 把label_z和other_z在第二个维度上拼接起来，并赋值给z
+        for idx in range(dim):  # 循环dim次
+            traversals = torch.linspace(-gap, gap, steps=n)  # 生成一个在[-gap, gap]区间上均匀分布的张量，并赋值给traversals
+            z_new = z.clone()  # 复制z，并赋值给z_new
+            z_new[:, idx] = traversals  # 把traversals赋值给z_new的第idx个维度
+            with torch.no_grad():  # 不计算梯度
+                sample[n * idx:(n * (idx + 1)), :, :, :] = self.decoder(z_new)  # 调用self.decoder，得到生成的图像，并赋值给sample的相应位置
+        return sample  # 返回sample
+
+    def forward(self, x=None, z=None, recon=False, infer_mean=True):
+        # recon_mean is used for gaussian decoder which we do not use here.
+        # Training Mode
+        if x is not None and z is None:  # 如果x和z都不是None
+            if self.enc_dist == 'gaussian':  # 如果self.enc_dist是'gaussian'
+                z_mu, z_logvar = self.encoder(x)  # 调用self.encoder，得到隐变量的均值和对数方差，并赋值给z_mu和z_logvar
+                z_fake = reparameterize(z_mu, (z_logvar / 2).exp())
+            else:  # deterministic or implicit
+                z_fake = self.encoder(x)  # 调用self.encoder，得到隐变量，并赋值给z_fake
+
+            if 'scm' in self.prior_dist:  # 如果self.prior_dist中包含'scm'
+                # in prior
+                label_z = self.prior(z_fake[:, :self.num_label])  # 调用self.prior，得到z的前self.num_label个维度经过因果层后的输出，并赋值给label_z
+                other_z = z_fake[:, self.num_label:]  # 得到z的剩余维度，并赋值给other_z
+                z = torch.cat([label_z, other_z], dim=1)  # 把label_z和other_z在第二个维度上拼接起来，并赋值给z
+
+
+            x_fake = self.decoder(z)  # 调用self.decoder，得到生成的图像，并赋值给x_fake
+
+            if recon == True:
+                return x_fake
+
+            if 'scm' in self.prior_dist:  # 如果self.prior_dist中包含'scm'
+                if self.enc_dist == 'gaussian' and infer_mean:  # 如果self.enc_dist是'gaussian'并且infer_mean为真
+                    return z_fake, x_fake, z, z_mu, z_logvar  # 返回z_fake, x_fake, z, z_mu
+                else:  # 否则
+                    return z_fake, x_fake, z, None, z_logvar  # 返回z_fake, x_fake, z, None
+            return z_fake, x_fake, z_mu, z_logvar  # 返回z_fake, x_fake, z_mu
+
+
+
+        # Generation Mode
+        elif x is None and z is not None:  # 如果x是None而z不是None
+            # if 'scm' in self.prior_dist:  # 如果self.prior_dist中包含'scm'
+            #     label_z = self.prior(z[:, :self.num_label])  # 调用self.prior，得到z的前self.num_label个维度经过因果层后的输出，并赋值给label_z
+            #     other_z = z[:, self.num_label:]  # 得到z的剩余维度，并赋值给other_z
+            #     z = torch.cat([label_z, other_z], dim=1)  # 把label_z和other_z在第二个维度上拼接起来，并赋值给z
+            return self.decoder(z)  # 返回调用self.decoder后的结果
+
+    def log_density_gaussian(self, x, mu, logvar):
+        """
+        Computes the log pdf of the Gaussian with parameters mu and logvar at x
+        :param x: (Tensor) Point at whichGaussian PDF is to be evaluated
+        :param mu: (Tensor) Mean of the Gaussian distribution
+        :param logvar: (Tensor) Log variance of the Gaussian distribution
+        :return:
+        """
+        norm = - 0.5 * (math.log(2 * math.pi) + logvar)
+        log_density = norm - 0.5 * ((x - mu) ** 2 * torch.exp(-logvar))
+        return log_density
+
+    def loss_function(self,
+                      *args,
+                      **kwargs) -> dict:
+        """
+        Computes the VAE loss function.
+        KL(N(\mu, \sigma), N(0, 1)) = \log \frac{1}{\sigma} + \frac{\sigma^2 + \mu^2}{2} - \frac{1}{2}
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+        recons = args[0]
+        input = args[1]
+        mu = args[2]
+        log_var = args[3]
+        z = args[4]
+
+        weight = 1  # kwargs['M_N']  # Account for the minibatch samples from the dataset
+
+        recons_loss = F.mse_loss(recons, input, reduction='sum')
+
+        log_q_zx = self.log_density_gaussian(z, mu, log_var).sum(dim=1)
+
+        zeros = torch.zeros_like(z)
+        log_p_z = self.log_density_gaussian(z, zeros, zeros).sum(dim=1)
+
+        batch_size, latent_dim = z.shape
+        mat_log_q_z = self.log_density_gaussian(z.view(batch_size, 1, latent_dim),
+                                                mu.view(1, batch_size, latent_dim),
+                                                log_var.view(1, batch_size, latent_dim))
+
+        # Reference
+        # [1] https://github.com/YannDubs/disentangling-vae/blob/535bbd2e9aeb5a200663a4f82f1d34e084c4ba8d/disvae/utils/math.py#L54
+        dataset_size = (1 / weight) * batch_size  # dataset size
+        strat_weight = (dataset_size - batch_size + 1) / (dataset_size * (batch_size - 1))
+        importance_weights = torch.Tensor(batch_size, batch_size).fill_(1 / (batch_size - 1)).to(input.device)
+        importance_weights.view(-1)[::batch_size] = 1 / dataset_size
+        importance_weights.view(-1)[1::batch_size] = strat_weight
+        importance_weights[batch_size - 2, 0] = strat_weight
+        log_importance_weights = importance_weights.log()
+
+        mat_log_q_z += log_importance_weights.view(batch_size, batch_size, 1)
+
+        log_q_z = torch.logsumexp(mat_log_q_z.sum(2), dim=1, keepdim=False)
+        log_prod_q_z = torch.logsumexp(mat_log_q_z, dim=1, keepdim=False).sum(1)
+
+        mi_loss = (log_q_zx - log_q_z).mean()
+        tc_loss = (log_q_z - log_prod_q_z).mean()
+        kld_loss = (log_prod_q_z - log_p_z).mean()
+
+        # kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1), dim = 0)
+
+        if self.training:
+            self.num_iter += 1
+            anneal_rate = min(0 + 1 * self.num_iter / self.anneal_steps, 1)
+        else:
+            anneal_rate = 1.
+
+        loss = recons_loss / batch_size + \
+               self.alpha * mi_loss + \
+               weight * (self.beta * tc_loss +
+                         anneal_rate * self.gamma * kld_loss)
+
+        return {'loss': loss,
+                'Reconstruction_Loss': recons_loss,
+                'KLD': kld_loss,
+                'TC_Loss': tc_loss,
+                'MI_Loss': mi_loss}
